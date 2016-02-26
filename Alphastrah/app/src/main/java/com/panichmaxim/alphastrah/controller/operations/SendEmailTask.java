7164fd2cca5ccb834faae0f1336f3d7059c129b8
Дзагуров Максим <panichmax@gmail.com>
2016-02-26 17:20:38 +0300

    Отправка письма через почтовый клиент
    Важно:
    Just click below link to disable security check
    google.com/settings/security/lesssecureapps

---
+++ Alphastrah/app/src/main/java/com/panichmaxim/alphastrah/controller/operations/SendEmailTask.java
@@ -0,0 +1,47 @@
+package com.panichmaxim.alphastrah.controller.operations;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.panichmaxim.alphastrah.App;
+import com.panichmaxim.alphastrah.utils.GMailSender;
+
+public class SendEmailTask extends AsyncTask<Void, Void, Void> {
+
+    private ProgressDialog pd;
+    private String subject;
+    private String body;
+    private Context ctx;
+
+    public SendEmailTask(String subject, String body, Context ctx) {
+        this.subject = subject;
+        this.body = body;
+        this.ctx = ctx;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        pd = ProgressDialog.show(ctx, null, "Загрузка", true, false);
+    }
+
+    @Override
+    protected Void doInBackground(Void... arg0) {
+        try {
+            GMailSender sender = new GMailSender("makar8883@gmail.com", "9303443pm");
+            sender.sendMail(subject,
+                    body,
+                    "maxim6665@yandex.ru");
+        } catch (Exception e) {
+            Log.e("SendMail", e.getMessage(), e);
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+        pd.dismiss();
+    }
+
+}
